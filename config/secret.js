c8423a6183eac1f843e9a83837a5cb5bfbe3443a
BradHemmerick <Brad.hemmerick13@gmail.com>
2018-03-29 14:40:17 -0500

    simple social media almost complete

---
+++ config/secret.js
@@ -0,0 +1,5 @@
+module.exports = {
+  database: 'mongodb://admin:password@ds115219.mlab.com:15219/chripy',
+  secret: "Open up your mind, let your fantasies unwind In this darkness which you know you cannot fight"
+
+}
